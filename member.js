36d3171bfcebf830eb4d5e740503b01af1e8e01c
Roman Kalitovskyi <110119358+RomanKalitovskyi@users.noreply.github.com>
2023-09-30 10:37:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    let skills = ["HTML", "CSS", "JavaScript", "PHP"];
+    let member = {
+        name: "John",
+        age: 20,
+        skills: skills
+    };
+    return member.skills;
+}
